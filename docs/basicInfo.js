41b96c790d267cb537ebac6bf02cb7d80d08f20e
Amadou <atidiane.camara@gmail.com>
2022-07-11 18:09:58 +0200

    setup openapi config

---
+++ docs/basicInfo.js
@@ -0,0 +1,12 @@
+module.exports = {
+    openapi: '3.1.0',
+    info: {
+        title: 'Simple user manager API',
+        description: 'User manager API',
+        version: '1.0.0',
+        contact: {
+            name: 'John DOE',
+            email: 'johndoe@email.com',
+        },
+    },
+};
